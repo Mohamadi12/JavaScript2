bb51bb893e2afdc27c7b4b4d20496f6b2809181a
Mohamadi12 <adilnana78@gmail.com>
2023-10-11 17:43:09 +0100

    rrr

--- main.js
+++ main.js
@@ -1,1 +1,80 @@
-alert("Hey");
+const product=[
+    {
+        id:0,
+        image:'image/3.jpeg',
+        title:'z Flip foldable Mobile',
+        price:120,
+    },
+    {
+        id:2,
+        image:'image/4.jpeg',
+        title:'Air Pods Pro',
+        price:60,
+    },
+    {
+        id:3,
+        image:'image/2.jpeg',
+        title:'250D DSLR Camera',
+        price:230,
+    },
+    {
+        id:4,
+        image:'image/1.jpeg',
+        title:'Head Phones',
+        price:100,
+    }
+];
+const categories=[...new Set(product.map((item)=>{
+    return item
+}))]
+let i=0;
+document.getElementById('root').innerHTML=categories.map((item)=>{
+    var {image,title,price}=item;
+    return(
+        `<div classe='box'>
+             <div class='img-box'>
+                <img class='images' src=${image}></img>
+              </div>  
+        
+        <div classe='bottom'>
+          <p>$ ${title}</p>
+               <h2> $ ${price}.00</h2>`+
+        "<button onclick='addtocart("+(i++)+")'>Add to cart</button>"+
+        `</div>
+        </div>`
+    )
+}).join('')
+
+var cart=[];
+function addtocart(a){
+    cart.push({...categories[a]});
+    displaycart();
+}
+function delElement(a){
+    cart.splice(a,1);
+    displaycart();
+}
+function displaycart(a){
+    let j=0, total=0;
+    document.getElementById("count").innerHTML=cart.length;
+    if(cart.length==0){
+        document.getElementById('cartItem').innerHTML="Your cart is empty";
+        document.getElementById("total").innerHTML="$ "+0+".00";
+    }
+    else{
+        document.getElementById('cartItem').innerHTML=cart.map((item)=>{
+            var {image,title,price}=item;
+            total=total+price;
+            document.getElementById("total").innerHTML="$ "+total+".00";
+            return(
+                `<div classe='cart-item'>
+             <div class='row-img'>
+                <img class='rowimg' src=${image}></img>
+        </div>
+        <p style='font-size:12px;'>${title}</p>
+        <h2 style='font-size:15px;'> $ ${price}.00</h2>`+
+        "<i class='fa-solid fa-trash'onclick='delElement("+ (j++) +")'></i</div> </div>"
+            );
+        }).join('');
+    }
+}
